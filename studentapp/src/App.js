b21352f8b132414fc505c789d8adc97d08baa631
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2024-11-19 17:20:41 +0000

    add buttons to change font size

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import StudentTable from './components/StudentTable';
 import { students } from './data/StudentsDb';
+import { useState } from 'react';
 
 function App() {
+
+  const [fontSize, setFontSize] = useState(16);
+
+  const handleFontSizeChange = (size) => {
+    setFontSize(size);
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{ fontSize: `${fontSize}px` }}>
       <h2>Student Information Portal</h2>
+      <hr/>
+      <p align="left">Font Size: 
+        <button onClick={() => handleFontSizeChange(16)}>S</button>
+        <button onClick={() => handleFontSizeChange(20)}>M</button>
+        <button onClick={() => handleFontSizeChange(24)}>L</button>
+      </p>
+      
       <hr/>
       <table width="100%">
         <tbody>
